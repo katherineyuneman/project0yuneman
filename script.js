64f159b4b71ec3371bbd7d2dc330ae46a0c0c0a0
KatherineYuneman <kyuneman@gmail.com>
2021-10-10 00:41:09 -0400

    added sidebar menu and js for visible/hidden text

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//transform: rotate(45deg);
-
+//will flip and unflip the round photo upon mouseover/mouseout
 const   img1 = document.getElementById("img1"),
         img2 = document.getElementById("img2");
 
@@ -20,4 +19,39 @@ const   img1 = document.getElementById("img1"),
 
         }          
         onmouseout;
+
+
         
+//show text based on button selection
+
+      
+        const button1 = document.getElementById("button1"),
+        button2 = document.getElementById("button2"),
+        aboutTitle = document.getElementById ("aboutTitle"),
+        aboutText = document.getElementById("aboutText");
+        socialTitle = document.getElementById("socialTitle");
+        socialText =document.getElementById("socialText");
+        
+
+        button1.addEventListener("click", function(e){
+            aboutTitle.style.visibility = "visible",
+            aboutText.style.visibility = "visible"
+            socialTitle.style.visibility = "hidden",
+            socialText.style.visibility = "hidden"
+        });
+
+        button1.click = function (e){
+
+        };
+
+        button2.addEventListener("click", function(e){
+            socialTitle.style.visibility = "visible",
+            socialText.style.visibility = "visible"
+            aboutTitle.style.visibility = "hidden",
+            aboutText.style.visibility = "hidden"
+            
+        });
+
+        button2.click = function (e){
+
+        };
