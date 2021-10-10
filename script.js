75a5d6fdbeb67ce72915bfc80379ce81b2b8b54f
KatherineYuneman <kyuneman@gmail.com>
2021-10-09 23:14:54 -0400

    javaScript with hover eventlistener for photo

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-
 //transform: rotate(45deg);
+
+const   img1 = document.getElementById("img1"),
+        img2 = document.getElementById("img2");
+
+        img1.addEventListener("mouseover", function(e) {
+            img1.style.display = "none";
+            img2.style.display = "block";
+          });
+        img1.onmouseover = function(e){
+
+        };
+        onmouseover;
+
+        img1.addEventListener("mouseout", function(e) {
+            img2.style.display = "none";
+            img1.style.display = "block";
+        });
+        img1.onmouseout = function (e) {
+
+        }          
+        onmouseout;
+        
